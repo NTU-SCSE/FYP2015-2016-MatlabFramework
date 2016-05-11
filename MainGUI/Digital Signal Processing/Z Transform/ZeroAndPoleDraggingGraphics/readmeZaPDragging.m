6ef508728d5b33febe148e8263434aac24530142
Nonny Florentine <nonny-florentine@hotmail.com>
2016-05-11 12:03:45 +0800

    Create readmeZaPDragging.m

---
+++ MainGUI/Digital Signal Processing/Z Transform/ZeroAndPoleDraggingGraphics/readmeZaPDragging.m
@@ -0,0 +1,3 @@
+<h2>Zero and Pole Dragging Graphics</h2>
+
+<p>The Z-Transform zero and pole dragging graphics app is an application where user can drag  the zeros and poles in the graph and the magnitude graph will update automatically.</p>
